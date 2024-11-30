7a0c62d3d8a79c0df4a2da55d5e82714c69f4875
AlexApostol <apostol516@gmail.com>
2024-11-30 14:59:35 +0100

    responsive menu

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -11,6 +11,7 @@ const Actions = {
 export function Navbar() {
   const [action, setAction] = useState(Actions.PREVIEW);
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const [navVisibile, setNavVisible] = useState(false);
   const [menuItems, setMenuItems] = useState({
     home: "Home",
     about: "About",
@@ -20,6 +21,10 @@ export function Navbar() {
     materials: "Dissemination Materials",
     contact: "Contact",
   });
+
+  const toggleNav = () => {
+    setNavVisible(!navVisibile);
+  };
 
   const handleOnClickPreview = () => {
     setAction((prev) =>
@@ -39,32 +44,51 @@ export function Navbar() {
   };
 
   return (
-    <nav className="navbar">
+    <header>
+      <link
+        rel="stylesheet"
+        href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css"
+      ></link>
       <div className="navbar-logo">
         <LogoUploader
           className="logo-image"
           isPreviewing={action === Actions.EDIT}
         />
       </div>
-      <ul className="navbar-links" onClick={openEditMenuModal}>
-        {Object.entries(menuItems).map(([key, name]) => (
-          <li key={key}>
-            <a href="#">{name}</a>
-          </li>
-        ))}
-      </ul>
+      <button
+        className={navVisibile ? "abrir-menu-visible" : "abrir-menu"}
+        onClick={toggleNav}
+      >
+        <i class="bi bi-list"></i>
+      </button>
+      <nav className={navVisibile ? `nav.visible` : `navbar`}>
+        <ul className="navbar-links" onClick={openEditMenuModal}>
+          <button
+            className={navVisibile ? "cerrar-menu-visible" : "cerrar-menu"}
+            onClick={toggleNav}
+          >
+            {" "}
+            Cerrar Menu
+          </button>
+          {Object.entries(menuItems).map(([key, name]) => (
+            <li key={key}>
+              <a href="#">{name}</a>
+            </li>
+          ))}
+        </ul>
+        {isModalOpen && (
+          <EditMenuModal
+            menuItems={menuItems}
+            onSave={handleSave}
+            onClose={() => setIsModalOpen(false)}
+          />
+        )}
+      </nav>
       <div className="navbar-signup">
         <button className="preview-button" onClick={handleOnClickPreview}>
           {action}
         </button>
       </div>
-      {isModalOpen && (
-        <EditMenuModal
-          menuItems={menuItems}
-          onSave={handleSave}
-          onClose={() => setIsModalOpen(false)}
-        />
-      )}
-    </nav>
+    </header>
   );
 }
